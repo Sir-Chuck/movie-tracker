523566e8a51dd5378cf1528e06c03925fa6add87
Sir-Chuck <Bnw1271@gmail.com>
2025-06-15 11:19:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,13 @@ st.markdown("""
         letter-spacing: 2px;
         font-weight: bold;
     }
-    .subtitle span:nth-child(1) { color: #f27802; }
-    .subtitle span:nth-child(2) { color: #2e0854; }
-    .subtitle span:nth-child(3) { color: #7786c8; }
-    .subtitle span:nth-child(4) { color: #708090; }
-    .subtitle span:nth-child(5) { color: #b02711; }
+    .subtitle span:nth-child(1) { color: #2a2a2a; }
+    .subtitle span:nth-child(2) { color: #2a2a2a; }
+    .subtitle span:nth-child(3) { color: #f27802; }
+    .subtitle span:nth-child(4) { color: #2e0854; }
+    .subtitle span:nth-child(5) { color: #7786c8; }
+    .subtitle span:nth-child(6) { color: #708090; }
+    .subtitle span:nth-child(7) { color: #b02711; }
 </style>
 """, unsafe_allow_html=True)
 
